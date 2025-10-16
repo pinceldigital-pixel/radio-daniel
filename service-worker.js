2dc834037ac17186b5c98c0b10f43605b7867772
pinceldigital-pixel <pinceldigital@gmail.com>
2025-10-15 23:03:43 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,23 @@
-const CACHE_NAME = 'radio-pwa-cache-v2';
+const CACHE_NAME = 'radio-pwa-cache-v2'; // Versión actualizada para forzar la actualización de la caché
 const urlsToCache = [
   '/',
-  'index.html'
+  '/index.html',
+  '/manifest.json',
+  '/9801466.png',
+  '/1000067018.png' // Ícono del temporizador añadido a la caché
 ];
+
+// Borrar cachés antiguas al activar el nuevo service worker
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(cacheName => cacheName !== CACHE_NAME)
+          .map(cacheName => caches.delete(cacheName))
+      );
+    })
+  );
+});
 
 self.addEventListener('install', event => {
   event.waitUntil(
@@ -12,30 +27,18 @@ self.addEventListener('install', event => {
         return cache.addAll(urlsToCache);
       })
   );
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', event => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-  return self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => {
-        return response || fetch(event.request);
+        // Si la respuesta está en la caché, la retornamos
+        if (response) {
+          return response;
+        }
+        // Si no, la buscamos en la red
+        return fetch(event.request);
       }
     )
   );
